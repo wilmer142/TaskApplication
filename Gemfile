052273a7b1a282425ece57beb6536e7aa00c2248
wilmer142 <wilmercollege@gmail.com>
2018-01-21 14:01:18 -0500

    Implementing bootstrap for style and configure routes

--- Gemfile
+++ Gemfile
@@ -25,20 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 #Use devise for authentication
 gem 'devise' 
-
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-end
+#Use bootstrap for style
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
 Rails.application.routes.draw do
   
-  get 'welcomes/index'
+  #Routes for users
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      :sessions => 'users/sessions',
+      :registrations => "users/registrations",
+      :passwords => "users/passwords"
+    }
+  end
 
-  get 'tasks/index'
-
-  get 'activities/index'
-
-  #Routes for devise
-  devise_for :users
-
-  #Roots for authenticated and authenticated users
+  #Routes for authenticated and authenticated users
   devise_scope :user do
     authenticated :user do
-      root 'welcomes#index'
+      root 'tasks#index'
     end
-
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+
+  #Routes for tasks and activities
+  resources :taks do
+    resources :activities
+  end
+
+  #Redirect non-existent routes to home
+  get '*path' => redirect('/')
 
 end
